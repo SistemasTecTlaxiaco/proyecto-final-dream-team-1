2aa5fad6364849eb372a9f66a603f8af0dd71104
DuilioAquino10 <79931792+DuilioAquino10@users.noreply.github.com>
2021-07-08 11:11:06 -0500

    Add files via upload

---
+++ SemoviCitas/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace SemoviCitas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.conectarabd();                            
+            }
+            catch
+            {
+                MessageBox.Show("ERROR AL CONECTAR CON LA BASE DE DATOS");
+            }
+        }
+
+        private void buttonFecha_Click(object sender, EventArgs e)
+        {
+            textBoxFecha.Text = monthCalendar1.SelectionStart.Year.ToString() + "-" +
+                                monthCalendar1.SelectionStart.Month.ToString() + "-" +
+                                monthCalendar1.SelectionStart.Day.ToString();
+        }
+
+        
+        private void Generar_Click(object sender, EventArgs e)
+        {
+            string insertar = "INSERT INTO citas(nombre,departamento,correo,fecha,hora)values(@nombre,@departamento,@correo,@fecha,@hora)";
+            MySqlCommand ins = new MySqlCommand(insertar, conexion.conectarabd());
+            ins.Parameters.AddWithValue("@nombre", textBoxNombre.Text);
+            ins.Parameters.AddWithValue("@departamento", comboBoxDepartamento.Text);
+            ins.Parameters.AddWithValue("@correo", textBoxCorreo.Text);
+            ins.Parameters.AddWithValue("@fecha", textBoxFecha.Text);
+            ins.Parameters.AddWithValue("@hora", comboBoxHora.Text);
+            ins.ExecuteNonQuery();
+
+            MessageBox.Show("Nombre: " + textBoxNombre.Text +
+                           "\nDepartamento: " + comboBoxDepartamento.SelectedItem +
+                            "\nCorreo: " + textBoxCorreo.Text +
+                            "\nHora: " +  comboBoxHora.Text +
+                            "\nFecha: " + monthCalendar1.SelectionStart.Day.ToString() + "/" +
+                                          monthCalendar1.SelectionStart.Month.ToString() + "/" +
+                                          monthCalendar1.SelectionStart.Year.ToString());
+
+            textBoxNombre.Clear();
+            comboBoxDepartamento.ResetText();
+            textBoxCorreo.Clear();
+            textBoxFecha.Clear();
+            comboBoxHora.ResetText();
+
+        }
+
+        private void cancelar_Click(object sender, EventArgs e)
+        {
+            textBoxNombre.Clear();
+            comboBoxDepartamento.ResetText();
+            textBoxCorreo.Clear();
+            textBoxFecha.Clear();
+            comboBoxHora.ResetText();
+
+        }
+
+      
+        private void citasGeneradasToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Form formulario2 = new Form2();
+            formulario2.Show();
+        }
+
+        private void mostrarTodasLasCitasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form formulario3 = new Form3();
+            formulario3.Show();
+        }
+
+        private void cancelarCitaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form formulario4 = new Form4();
+            formulario4.Show();
+        }
+
+        
+    }
+}
---
+++ SemoviCitas/Form3.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace SemoviCitas
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonFecha = new System.Windows.Forms.Button();
+            this.textBoxFecha = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxCorreo = new System.Windows.Forms.TextBox();
+            this.comboBoxHora = new System.Windows.Forms.ComboBox();
+            this.comboBoxDepartamento = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(174, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 179);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(242, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GENERAR LISTA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(534, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generar cambios";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonFecha
+            // 
+            this.buttonFecha.Location = new System.Drawing.Point(461, 382);
+            this.buttonFecha.Name = "buttonFecha";
+            this.buttonFecha.Size = new System.Drawing.Size(114, 21);
+            this.buttonFecha.TabIndex = 23;
+            this.buttonFecha.Text = "Seleccionar fecha";
+            this.buttonFecha.UseVisualStyleBackColor = true;
+            this.buttonFecha.Click += new System.EventHandler(this.buttonFecha_Click);
+            // 
+            // textBoxFecha
+            // 
+            this.textBoxFecha.Location = new System.Drawing.Point(592, 382);
+            this.textBoxFecha.Name = "textBoxFecha";
+            this.textBoxFecha.Size = new System.Drawing.Size(117, 20);
+            this.textBoxFecha.TabIndex = 22;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(461, 203);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 21;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(242, 205);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(148, 20);
+            this.textBoxNombre.TabIndex = 20;
+            // 
+            // textBoxCorreo
+            // 
+            this.textBoxCorreo.Location = new System.Drawing.Point(242, 296);
+            this.textBoxCorreo.Name = "textBoxCorreo";
+            this.textBoxCorreo.Size = new System.Drawing.Size(148, 20);
+            this.textBoxCorreo.TabIndex = 19;
+            // 
+            // comboBoxHora
+            // 
+            this.comboBoxHora.FormattingEnabled = true;
+            this.comboBoxHora.Items.AddRange(new object[] {
+            "10:00",
+            "11:00",
+            "12:00",
+            "13:00",
+            "14:00",
+            "15:00"});
+            this.comboBoxHora.Location = new System.Drawing.Point(269, 335);
+            this.comboBoxHora.Name = "comboBoxHora";
+            this.comboBoxHora.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxHora.TabIndex = 18;
+            // 
+            // comboBoxDepartamento
+            // 
+            this.comboBoxDepartamento.FormattingEnabled = true;
+            this.comboBoxDepartamento.Items.AddRange(new object[] {
+            "LICENCIAS",
+            "TENENCIA",
+            "EMPLACAMIENTO",
+            "CONCESIONES ",
+            "ALTA/BAJA DE VEHICULOS"});
+            this.comboBoxDepartamento.Location = new System.Drawing.Point(269, 249);
+            this.comboBoxDepartamento.Name = "comboBoxDepartamento";
+            this.comboBoxDepartamento.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDepartamento.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(177, 343);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Hora";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Correo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Departamento";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Nombre";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(278, 382);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(112, 20);
+            this.textBoxId.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(180, 385);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Identificador";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SemoviCitas.Properties.Resources.semovi_nota3;
+            this.pictureBox2.Location = new System.Drawing.Point(328, 515);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(265, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 45;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(887, 634);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.buttonFecha);
+            this.Controls.Add(this.textBoxFecha);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxCorreo);
+            this.Controls.Add(this.comboBoxHora);
+            this.Controls.Add(this.comboBoxDepartamento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.Text = "Modificar";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonFecha;
+        private System.Windows.Forms.TextBox textBoxFecha;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxCorreo;
+        private System.Windows.Forms.ComboBox comboBoxHora;
+        private System.Windows.Forms.ComboBox comboBoxDepartamento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
